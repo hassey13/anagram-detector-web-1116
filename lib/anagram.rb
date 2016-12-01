c5deff01809eefea1169a57f7d75f6309d7a670d
Eric Hasselbring <ehasselbring@gmail.com>
2016-12-01 03:14:18 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+      attr_accessor :word
+
+      def initialize(word)
+            @word = word
+      end
+      
+      def match(array)
+            array.select do |potential_match|
+                  potential_match.split("").sort == word.split("").sort
+            end
+      end
+end
